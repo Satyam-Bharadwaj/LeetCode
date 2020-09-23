b1cee98da96fc544f26641b959885da6340ce588
Satyam-Bharadwaj <satyam.bharadwaj.96@gmail.com>
2020-09-23 22:48:45 +0530

    Daily Challenge, Reverse LL

---
+++ Linked-List/Reverse Linked List.java
@@ -0,0 +1,33 @@
+/**
+ * 
+ * https://leetcode.com/explore/learn/card/linked-list/219/classic-problems/1205/
+ * 
+ * 
+ */
+
+
+ /**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+
+    
+    public ListNode reverseList(ListNode head) {
+        if( head ==null ||head.next==null) {
+            return head;
+                            
+                            }
+        ListNode reversed = reverseList(head.next);
+        head.next.next = head;
+        head.next=null;
+        return reversed;
+        
+    }
+}
